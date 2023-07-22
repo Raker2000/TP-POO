ab5398827b6e5a6f52c7657945a77f51c536abe8
Raker2000 <85421237+Raker2000@users.noreply.github.com>
2023-07-22 17:51:47 -0300

    Update tablero.h

--- TP2-POO/tablero.h
+++ TP2-POO/tablero.h
@@ -4,8 +4,15 @@
 
 class Tablero
 {
+private:
+	int filas, columnas;
+	int** tablero;//la clase tablero contiene a la matriz tablero
 public:
     Tablero();
+	void generarTablero(int f, int c);
+	void setTablero(int f, int c);
+	void resetTablero();
+	int** getTablero();
 };
 
 #endif // TABLERO_H
