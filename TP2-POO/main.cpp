06b3ef3ea59ac72d188d059a69a43f4e3e69c0d7
Raker2000 <85421237+Raker2000@users.noreply.github.com>
2023-07-24 16:35:34 -0300

    Update main.cpp

--- TP2-POO/main.cpp
+++ TP2-POO/main.cpp
@@ -1,8 +1,22 @@
 #include <QCoreApplication>
+#include <tablero.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    int filas,columnas;
+
+    cout<<"Filas: "; cin>>filas;
+    cout<<"Columnas: "; cin>>columnas;
+
+    Tablero* t = new Tablero(filas,columnas);
+
+    t->mostrarTablero();
 
     return a.exec();
 }
